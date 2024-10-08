2f6e6817c09a3a8dcc1a365f3b04e468487433b7
KrystianFontys <k.surowiec@student.fontys.nl>
2024-10-08 11:26:53 +0200

    create the camera class and add the methods to it

---
+++ Camera.py
@@ -0,0 +1,22 @@
+
+
+class Camera:
+    def __init__(self, resolution, channels, address, white_balance, camera_error, image_path, product_detected, product_list,product_dict, product_colour):
+        self.resolution = resolution
+        self.channels = channels
+        self.address = address
+        self.white_balance = white_balance
+        self.camera_error = camera_error
+        self.image_path = image_path
+        self.product_detected = product_detected
+        self.product_list = product_list
+        self.product_dict = product_dict
+        self.product_colour = product_colour
+
+    def get_image():
+        return None
+
+    def process_image(self.image_path): #FIXME:
+        return None
+
+    
