12b0a0ad4478297ecfa9d1c60ccb220b3cc6b40f
KrystianFontys <k.surowiec@student.fontys.nl>
2024-10-08 12:03:08 +0200

    create the conveyor class and added method to it

---
+++ Conveyor.py
@@ -0,0 +1,10 @@
+
+
+class Conveyor:
+    def __init__(self, velocity, conveyor_serial_timeout):
+        self.velocity = velocity
+        self.conveyor_serial_timeout = conveyor_serial_timeout
+    
+    def move(velocity):
+        return None
+        
