46e936f79205f69fad7f501375fe078e99a4839b
KrystianFontys <k.surowiec@student.fontys.nl>
2024-10-08 11:06:22 +0200

    make class + add some methods

---
+++ Product.py
@@ -0,0 +1,21 @@
+
+
+
+class Product:
+
+    def __init__ (self, ID, product_type, product_colour, pixel_pos, product_pos):
+
+        self.ID = ID
+        self.product_type = product_type
+        self.product_colour = product_colour
+        self.pixel_pos = pixel_pos
+        self.product_pos = product_pos
+
+    def get_product_ID():
+        return self.ID
+
+    def get_product_type():
+        return self.product_type
+
+    
+    
