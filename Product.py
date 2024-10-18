45df986e1f71ae577d73cf20bd3a49e9558dac45
thesemsea <isabelasanunes@gmail.com>
2024-10-18 13:19:02 +0200

    made atributtes private; fixed methods with 'self' parameters

--- Product.py
+++ Product.py
@@ -7,27 +7,21 @@ Class for products.
 
 class Product:
 
-    def __init__ (self, ID, product_type, product_colour, pixel_pos, product_pos):
+    def __init__ (self, ID, product_type, product_colour, pixel_pos):
+        self.__ID = ID                     
+        self.__product_type = product_type  
+        self.__product_colour = product_colour  
+        self.__pixel_pos = pixel_pos       
 
-        self.ID = ID
-        self.product_type = product_type
-        self.product_colour = product_colour
-        self.pixel_pos = pixel_pos
-        self.product_pos = product_pos
+    # getter methods
+    def get_product_ID(self):
+        return self.__ID
 
-    def get_product_ID():
-        return self.ID
+    def get_product_type(self):
+        return self.__product_type
 
-    def get_product_type():
-        return self.product_type
+    def get_product_colour(self):
+        return self.__product_colour
 
-    def get_product_colour():
-        return self.product_colour
-
-    def get_pixel_pos():
-        return self.pixel_pos
-
-    def get_product_pos():
-        return self.product_pos
-
-    
+    def get_pixel_pos(self):
+        return self.__pixel_pos
