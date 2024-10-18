dc6700c90b67f61f84af3fda1f3ba95411646fab
audreyemalieu <a.emalieungafa@student.fontys.nl>
2024-10-18 13:50:57 +0200

    Added self parameteer to Product classs

--- GUI.py
+++ GUI.py
@@ -1,7 +1,9 @@
-#
-
+#here the possible shapes to be picked are shown
+#and the user can select one for the entire process.
+#You could let the UI randomly (color and shape) select which shapes (at least three) must be placed on the loading area 
+from Product import *
 class GUI:
-    def __init__(self, resolution, duration, product_list, product_selection):
+    def __init__(self, resolution, duration,product, product_list, product_selection):
         self.resolution = resolution
         self.duration = duration
         self.product_list = product_list
@@ -14,15 +16,15 @@ class GUI:
         placeholder = 0
         return placeholder #TODO:
 
-    def toggle_product_selection(product_selection):
-        return None
     
     def display_error():
         return ("Message") #TODO:
 
-    def instantiate_product():
+    def instantiate_product(self):
+        self.product=Product(ID, product_type, product_colour, pixel_pos, product_pos)
         return None
 
     def display_products():
         return None
-        
+    def toggle_product_selection(product_selection):
+        return None  
