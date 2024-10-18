d361bc13b7ef1063ecc63cb91b2a76c297f2f6be
thesemsea <isabelasanunes@gmail.com>
2024-10-18 14:04:25 +0200

    made Dobot class inherit from DoBotArm

--- Dobot.py
+++ Dobot.py
@@ -3,23 +3,28 @@ Class Dobot:
 Pick and place items with a Dobot.
 """
 
-class Dobot:
-    def __init__(self, current_pos, current_x, current_y, current_z, desired_pos, desired_x, desired_y, desired_z, home_pos, error, moving_x, moving_y, moving_z, end_pos, dobot_serial_timeout):
-        self.current_pos = current_pos
-        self.current_x = current_x
-        self.current_y = current_y
-        self.current_z = current_z
-        self.desired_pos = desired_pos
-        self.desired_x = desired_x
-        self.desired_y = desired_y
-        self.desired_z = desired_z
-        self.home_pos = home_pos
-        self.error = error
-        self.moving_x = moving_x
-        self.moving_y = moving_y
-        self.moving_z = moving_z
-        self.end_pos = end_pos
-        self.dobot_serial_timeout = dobot_serial_timeout
+import DoBotArm
+
+class Dobot(DoBotArm):
+    def __init__(self, port, homeX, homeY, homeZ, home = False, homingWait = True):
+         
+         super().__init__(self, port, homeX, homeY, homeZ, home = False, homingWait = True)
+
+    self.current_pos = current_pos
+    self.current_x = current_x
+    self.current_y = current_y
+    self.current_z = current_z
+    self.desired_pos = desired_pos
+    self.desired_x = desired_x
+    self.desired_y = desired_y
+    self.desired_z = desired_z
+    self.home_pos = home_pos
+    self.error = error
+    self.moving_x = moving_x
+    self.moving_y = moving_y
+    self.moving_z = moving_z
+    self.end_pos = end_pos
+    self.dobot_serial_timeout = dobot_serial_timeout
 
     def go_to_pos():# no Z movement
 
