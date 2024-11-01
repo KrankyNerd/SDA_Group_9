ea9d18d27815a7a70d225386b806adf8e3044b5a
audreyemalieu <a.emalieungafa@student.fontys.nl>
2024-11-01 15:28:32 +0100

    1. Added counter to assgn IDs to proudcts in increasing order.
    2. Added pixel_posx and pixel_posy

--- Product.py
+++ Product.py
@@ -2,13 +2,16 @@
 
 
 class Product:
+    product_counter = 1  # Class variable to keep track of the ID count
+   
+    def __init__ (self, ID, product_type, product_colour, pixel_posx, pixel_posy,product_pos):
 
-    def __init__ (self, ID, product_type, product_colour, pixel_pos, product_pos):
-
-        self.ID = ID
+        self.ID = Product.product_counter  # Assign the current count as ID
+        Product.product_counter += 1  # Increment the counter for the next product
         self.product_type = product_type
         self.product_colour = product_colour
-        self.pixel_pos = pixel_pos
+        self.pixel_posy = pixel_posy
+        self.product_posx = pixel_posx
         self.product_pos = product_pos
 
     def get_product_ID(self):
