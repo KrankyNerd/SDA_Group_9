8a8c0698686dcd67d8b235413663fb6dc31cb9ae
thesemsea <isabelasanunes@gmail.com>
2024-11-01 15:09:30 +0100

    endEffector class made

--- EndEffector.py
+++ EndEffector.py
@@ -1,14 +1,21 @@
+"""Class End Effector:
 
+Control a boolean suction end-effector. 
+"""
 
 class EndEffector:
-    def __init__(self, state):
+    def __init__(self, state: bool = False):
+        """Initialize a suction end-effector object.
+    
+        :param: 
+        state of the suction end-effector.
+        """
         self.state = state
 
-    def toggle_state():
+    def toggle_state(self) -> None:
+        self.state = not self.state   
         return None
     
-    def get_state():
-        placeholder = 0
-        return placeholder #TODO:
-
+    def get_state(self) -> bool:
+        return self.state
     
