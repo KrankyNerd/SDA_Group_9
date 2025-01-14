25a7c4b57e35b2763b177b334f35dc8b699538c4
thesemsea <isabelasanunes@gmail.com>
2025-01-14 14:38:45 +0100

    flipping vertically now

--- Camera.py
+++ Camera.py
@@ -53,6 +53,7 @@ class Camera:
 
     def process_image(self, frame):
         frame = cv2.flip(frame, 1)
+        frame = cv2.flip(frame, 0)
         # Convert the captured image to grayscale
         grayimg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow("Gray Image", grayimg)
