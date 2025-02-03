1e3b3487b7353985d7660b43d705325c2b97a255
thesemsea <isabelasanunes@gmail.com>
2025-02-03 15:57:16 +0100

    yet again new values for conversion

--- dobotdevwithratio.py
+++ dobotdevwithratio.py
@@ -10,10 +10,10 @@ from Camera import Camera
 
 #constants
 homeX, homeY, homeZ = 170, 0, 30
-xpixel_to_cm = 16/219
-ypixel_to_cm = 16/227
+xpixel_to_cm = 219/16
+ypixel_to_cm = 227/16
 cm_to_dobot_x = 128/12.5
-cm_to_dobot_y = 133/12.5
+cm_to_dobot_y = -133/12.5
 #conveyorPos = 20, -220, 30
 
 #objects
