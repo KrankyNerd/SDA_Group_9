1a873ed3c05cb97671b353fd5e93818ee582e194
KrystianFontys <k.surowiec@student.fontys.nl>
2025-02-03 18:19:31 +0100

    working test 1

--- Testing1.py
+++ Testing1.py
@@ -79,14 +79,21 @@ for x, y in positions:
     dobot_x = 0.71 * x + 124
     dobot_y = -0.738 * y + 99.76
     ctrlDobot.moveArmXYZ(dobot_x, dobot_y, 30)
-    ctrlDobot.moveArmXYZ(dobot_x, dobot_y, -33.5)
+    ctrlDobot.moveArmXYZ(dobot_x, dobot_y, -34.0)
     #ctrlDobot.toggleSuction()
     #ctrlDobot.pickToggle(-31.5, True)
     time.sleep(2)
     ctrlDobot.toggleSuction(True)
-    ctrlDobot.moveArmXYZ(200, 45, 30)
+    ctrlDobot.moveArmXYZ(None, None, 30)
+    ctrlDobot.moveHome()
+    ctrlDobot.moveArmXYZ(None, -220, 30)
+    ctrlDobot.moveArmXYZ(100, -220, 30)
     time.sleep(2)
     ctrlDobot.toggleSuction(False)
+    ctrlDobot.moveArmXYZ(170, -220, 30)
+    ctrlDobot.moveArmXYZ(None, -220, 30)
+    ctrlDobot.moveHome()
+
     print(dobot_x)
     print(dobot_y)
     time.sleep(2)
