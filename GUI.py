72322858cc45ca9f4487db33a991dd8c5e1b7ad8
thesemsea <isabelasanunes@gmail.com>
2025-02-06 18:22:11 +0100

    this might work

--- GUI.py
+++ GUI.py
@@ -1,61 +1,52 @@
 """
 Class GUI:
-add_doc_strings_to_classes
 This class controls a Graphic User Interface.
-
 """
+
 import time
-from Product import *
-from Camera import *
+from Product import Product
+from Camera import Camera
+import cv2
+
 class GUI:
-    def __init__(self, resolution, duration, product_list, product_selection):
+    def _init_(self, resolution, duration, product_list, product_selection):
         self.resolution = resolution
         self.duration = duration
         self.product_list = product_list
-        self.product_selection = product_selection #bool to indicate product has been selected
+        self.product_selection = product_selection  # Boolean to indicate if a product has been selected
 
     def start_timer(self, duration):
         """
-        Start a timer in miliseconds.
+        Start a timer in milliseconds.
         
-        :param duration float: duration of timer.
-        :return: true if time has elapsed, false otherwise. 
+        :param duration: Duration of the timer.
+        :return: True if time has elapsed, False otherwise.
         """
-        start_time=0# when timer starts
-        current_time = time.time()
-        if current_time - start_time >= duration:
-            return True
-        else:
-            return False      
+        start_time = time.time()
+        return (time.time() - start_time) >= duration
 
-    def toggle_product_selection(self, product_selection):
+    def toggle_product_selection(self):
         """
-        Toggle the state of end-effector responsible for selecting a product.
+        Toggle the state of the product selection flag.
+        """
+        self.product_selection = not self.product_selection
 
-        :return: None.
+    def display_error(self, message="An error occurred."):
         """
-        product_selection =not product_selection
-        return None
-    
-    def display_error(self):
-        """
-        Print a especific error message to terminal.
+        Print a specific error message to the terminal.
         
-        :return: string. 
+        :param message: The error message to display.
         """
-    
-        return ("Message")
+        print(f"Error: {message}")
 
-    def instantiate_product(self,detected_shapesdata):#, product_dict):
+    def instantiate_product(self, detected_shapesdata):
         """
-        Instantiate a product and append it to the product list. 
-
-        :param product_dict: dictonary containing product data.
-        :return: None. 
-        """
-        product_counter =0 # Assign the current count as ID
+        Instantiate products from detected shapes data and append them to the product list.
         
-        for product_dict in  detected_shapesdata :
+        :param detected_shapesdata: List of dictionaries containing shape data.
+        """
+        product_counter = len(self.product_list)  # Continue ID from current list size
+        for product_dict in detected_shapesdata:
             product = Product(
                 ID=product_counter,
                 product_type=product_dict["product_type"],
@@ -63,37 +54,17 @@ class GUI:
                 pixel_posx=product_dict["pixel_posx"],
                 pixel_posy=product_dict["pixel_posy"]
             )
-            #product = Product(**product_dict)
             self.product_list.append(product)
-            product_counter += 1  # Increment the counter for the next product
-        
+            product_counter += 1
 
-        return self.product_list
     def display_products(self):
         """
-        Display all products in product_list on screen.
+        Display all products in the product list on the screen.
         """
-        for product in self.product_list:
-            print(f"Detected Product{product.ID}:{product.product_type}of colour {product.product_colour} at x:{product.pixel_posx} and y{product.pixel_posy}")
-        
+        if not self.product_list:
+            print("No products detected.")
+            return
 
-        return None
-    
-if __name__ == "__main__": 
-    myGUI= GUI(10,500,[],True)
-    camera= Camera()
-    #we need a loop so that multiple products can be created and appeneded
-   
-    while True: #not good, infinite loop     
-        frame=camera.get_image()
-        if frame is not None:
-            processed_image, detected_shapesdata = camera.process_image(frame)#detected_shapesdata =camera.process_image(frame)[1]
-            print (detected_shapesdata)
-            myGUI.instantiate_product(detected_shapesdata)
-            myGUI.display_products()
-            cv2.imshow("Processed Image", processed_image)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-    
-    
-    camera.release()
+        for product in self.product_list:
+            print(f"Product {product.ID}: {product.product_type}, Color: {product.product_colour}, "
+                  f"Position: ({product.pixel_posx}, {product.pixel_posy})")
