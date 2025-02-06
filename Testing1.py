b077e0e6e8c71018236e06632cd9d6e08b3c3471
thesemsea <isabelasanunes@gmail.com>
2025-02-06 19:00:21 +0100

    v

--- Testing1.py
+++ Testing1.py
@@ -53,11 +53,11 @@ ctrlDobot.toggleSuction = MethodType(toggleSuction, ctrlDobot)
 
 # ------------------- MAIN   START -------------------------
 
-ctrlDobot.SetConveyor(True, 15000)
+ctrlDobot.SetConveyor( 15000)
 time.sleep(3)
-ctrlDobot.SetConveyor(True, -15000)
+ctrlDobot.SetConveyor( -15000)
 time.sleep(3)
-ctrlDobot.SetConveyor(False)
+ctrlDobot.SetConveyor(0)
 
 
 
